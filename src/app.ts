32ba325320a59b285895f66a6d559246eec82a8e
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-07-10 13:28:58 +0300

    lsn 3 ESLint: TypeError: prettier.resolveConfig.sync is not a function
    БУЛА ПОМИЛКА вирішив змінив версію prettier з 3.0.0 на 2.8.8

--- src/app.ts
+++ src/app.ts
@@ -73,69 +73,66 @@
 // =========================================== 4 streams:
 //                ==== READ, WRITE, DUPLEX, TRANSFORM - !!! ====
 
-
-
-                                                //SERVER FRAMEWORK
+//SERVER FRAMEWORK
 // // // const zlib = require('node:zlib');
 
+import { Request, Response } from "express";
 
-const express = require('express');
-
-import {Request, Response} from "express";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require("express");
 
 const app = express();
 
 const users = [
   {
-    name: 'Oleh',
-  age: 20,
-  gender: 'male'
+    name: "Oleh",
+    age: 20,
+    gender: "male",
   },
   {
-    name: 'Katya',
+    name: "Katya",
     age: 22,
-    gender: 'female'
+    gender: "female",
   },
   {
-    name: 'Olha',
+    name: "Olha",
     age: 24,
-    gender: 'female'
+    gender: "female",
   },
   {
-    name: 'Oleh',
+    name: "Oleh",
     age: 25,
-    gender: 'male'
+    gender: "male",
   },
   {
-    name: 'Katya',
+    name: "Katya",
     age: 26,
-    gender: 'female'
+    gender: "female",
   },
   {
-    name: 'Oleh',
+    name: "Oleh",
     age: 29,
-    gender: 'male'
+    gender: "male",
   },
   {
-    name: 'Innokentiy',
+    name: "Innokentiy",
     age: 18,
-    gender: 'male'
+    gender: "male",
   },
   {
-    name: 'Olena',
+    name: "Olena",
     age: 44,
-    gender: 'female'
+    gender: "female",
   },
   {
-    name: 'Cocos',
+    name: "Cocos",
     age: 79,
-    gender: 'female'
+    gender: "female",
   },
-]
+];
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}))
-
+app.use(express.urlencoded({ extended: true }));
 
 // app.get('/', (req, res) => {
 // // // //  // console.log('Hello FROM /!')
@@ -145,57 +142,53 @@ app.use(express.urlencoded({extended: true}))
 // //  // })
 // })
 
-app.get('/users', (req: Request, res: Response) => {
+app.get("/users", (req: Request, res: Response) => {
   res.status(200).json(users);
-})
+});
 
-app.get('/users/:userId', (req: Request, res: Response) => {
-  const {userId} = req.params;
+app.get("/users/:userId", (req: Request, res: Response) => {
+  const { userId } = req.params;
   console.log(userId);
   res.status(200).json(users[+userId]);
-})
+});
 
-app.post('/users', (req: Request, res: Response) => {
+app.post("/users", (req: Request, res: Response) => {
   // console.log(req.body);
   users.push(req.body);
 
   res.status(201).json({
-    message: 'User created!'
-  })
-})
+    message: "User created!",
+  });
+});
 
-app.put('/users/:userId', (req: Request, res: Response) => {
-  const {  userId } = req.params;
+app.put("/users/:userId", (req: Request, res: Response) => {
+  const { userId } = req.params;
 
   users[+userId] = req.body;
 
   res.status(200).json({
-    message: 'User updated.',
-    data: users[+userId]
-  })
-})
+    message: "User updated.",
+    data: users[+userId],
+  });
+});
 
-app.delete('/users/:userId', (req: Request, res: Response) => {
-  const {userId} = req.params;
+app.delete("/users/:userId", (req: Request, res: Response) => {
+  const { userId } = req.params;
 
   users.splice(+userId, 1);
 
   res.status(200).json({
-    message: 'User deleted.',
-  })
-})
-
+    message: "User deleted.",
+  });
+});
 
 const PORT = 3999;
 
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT} 😂😂`)
-})
+  console.log(`Example app listening on port ${PORT} 😂😂`);
+});
 //
 //
 // =================================================================
-
-
-
 
 //                          ==== CRUD ====
