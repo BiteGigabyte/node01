eebc358ca55bbfa104d96905862d7f891b8827a1
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-07-15 22:30:41 +0300

    lsn 4 закінчив видалив трохи писали по різному, будем десь зараз запускати. Трохи змінили одне місце пробую 42:28

--- src/validators/user.validator.ts
+++ src/validators/user.validator.ts
@@ -6,17 +6,12 @@ import { EGenders } from "../enums/user.enum";
 export class UserValidator {
   static firstName = Joi.string().min(3).max(30).trim();
   static age = Joi.number().min(1).max(199).required();
-  static gender = Joi.valid(EGenders).required();
-  static email = Joi.string()
-    .regex(regexConstants.EMAIL)
-    .lowercase()
-    .trim();
-  static password = Joi.string()
-    .regex(regexConstants.PASSWORD)
-    .trim();
+  static gender = Joi.valid(...Object.values(EGenders)).required();
+  static email = Joi.string().regex(regexConstants.EMAIL).lowercase().trim();
+  static password = Joi.string().regex(regexConstants.PASSWORD).trim();
 
   static create = Joi.object({
-    firstName: this.firstName.required(),
+    name: this.firstName.required(),
     age: this.age,
     gender: this.gender,
     email: this.email.required(),
