3d765d75d5cc4d870e4ddb0cdc1f4a064579d32d
BiteGigabyte <121199599+BiteGigabyte@users.noreply.github.com>
2023-07-16 14:34:57 +0300

    lsn5 роблю завантаження 14:39 переписати з файлу route в файл user controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,9 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 
+import { ApiError } from "../errors";
 import { User } from "../models/User.mode";
 import { IUser } from "../types/user.type";
+import { UserValidator } from "../validators";
 
 class UserController {
   public async findAll(
@@ -15,6 +17,86 @@ class UserController {
       return res.json(users);
     } catch (e) {
       console.log(e);
+    }
+  }
+
+  public async create(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<IUser>> {
+    {
+      // console.log(req.body);
+      // users.push(req.body);
+
+      try {
+        const { error, value } = UserValidator.create.validate(req.body);
+
+        if (error) {
+          throw new ApiError(error.message, 400);
+        }
+
+        const createdUser = await User.create(value);
+
+        return res.status(201).json(createdUser);
+      } catch (e) {
+        next(e);
+      }
+    }
+  }
+
+  public async findById(req: Request, res: Response): Promise<Response<IUser>> {
+    try {
+      const user = await User.findById(req.params.id);
+
+      return res.json(user);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  public async updateById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<void> {
+    try {
+      const { userId } = req.params;
+
+      const { error } = UserValidator.update.validate(req.body);
+
+      if (error) {
+        throw new ApiError(error.message, 400);
+      }
+
+      // const updatedUser = await User.updateOne({ _id: userId }, { ...req.body });
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: userId },
+        { ...req.body },
+        { returnDocument: "after" }
+      );
+
+      res.status(200).json(updatedUser);
+    } catch (e) {
+      next(e);
+    }
+    // users[+userId] = req.body;
+  }
+
+  public async deleteById(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): Promise<Response<void>> {
+    try {
+      const { userId } = req.params;
+
+      // const updatedUser = await User.updateOne({ _id: userId }, { ...req.body });
+      await User.deleteOne({ _id: userId });
+
+      return res.sendStatus(200);
+    } catch (e) {
+      next(e);
     }
   }
 }
--- src/routers/user.router.ts
+++ src/routers/user.router.ts
@@ -1,16 +1,14 @@
 import { NextFunction, Request, Response, Router } from "express";
 
+import { userController } from "../controllers/user.controller";
 import { ApiError } from "../errors";
 import { User } from "../models/User.mode";
 import { IUser } from "../types/user.type";
 import { UserValidator } from "../validators";
-import {userController} from "../controllers/user.controller";
 
 const router = Router();
 
-router.get(
-  "/",
-  userController.findAll
+router.get("/", userController.findAll
   // async (req: Request, res: Response): Promise<Response<IUser[]>> => {
   //   try {
   //     const users = await User.find();
@@ -20,93 +18,11 @@ router.get(
   //   } catch (e) {
   //     console.log(e);
   //   }
-  // }
+  // });
 );
-
-router.get(
-  "/:id",
-  async (req: Request, res: Response): Promise<Response<IUser>> => {
-    try {
-      const user = await User.findById(req.params.id);
-
-      return res.json(user);
-    } catch (e) {
-      console.log(e);
-    }
-  }
-);
-
-router.post(
-  "/",
-  async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response<IUser>> => {
-    // console.log(req.body);
-    // users.push(req.body);
-
-    try {
-      const { error, value } = UserValidator.create.validate(req.body);
-
-      if (error) {
-        throw new ApiError(error.message, 400);
-      }
-
-      const createdUser = await User.create(value);
-
-      return res.status(201).json(createdUser);
-    } catch (e) {
-      next(e);
-    }
-  }
-);
-
-router.put(
-  "/:userId",
-  async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    try {
-      const { userId } = req.params;
-
-      const { error } = UserValidator.update.validate(req.body);
-
-      if (error) {
-        throw new ApiError(error.message, 400);
-      }
-
-      // const updatedUser = await User.updateOne({ _id: userId }, { ...req.body });
-      const updatedUser = await User.findOneAndUpdate(
-        { _id: userId },
-        { ...req.body },
-        { returnDocument: "after" }
-      );
-
-      res.status(200).json(updatedUser);
-    } catch (e) {
-      next(e);
-    }
-    // users[+userId] = req.body;
-  }
-);
-
-router.delete(
-  "/:userId",
-  async (
-    req: Request,
-    res: Response,
-    next: NextFunction
-  ): Promise<Response<void>> => {
-    try {
-      const { userId } = req.params;
-
-      // const updatedUser = await User.updateOne({ _id: userId }, { ...req.body });
-      await User.deleteOne({ _id: userId });
-
-      return res.sendStatus(200);
-    } catch (e) {
-      next(e);
-    }
-  }
-);
+router.post("/", userController.create);
+router.get("/:id", userController.findById);
+router.put("/:userId", userController.updateById);
+router.delete("/:userId", userController.deleteById);
 
 export const userRouter = router;
